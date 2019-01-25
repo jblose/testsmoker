6e202df3db95b534c68c879183d15a4cf57a8012
jblose <jamblose@gmail.com>
2019-01-25 16:18:50 -0500

    Intial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Set up using VirtualEnv
+$ cd smoker
+$ virtualenv venv
+
+Preserve environment:
+$ pip freeze > requirements.txt
+
+Installing environment:
+$ pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import yaml
+
+with open("tests.yml", 'r') as stream:
+    try:
+        testyaml = yaml.load(stream)
+        for x in testyaml['tests']:
+            test_name = x['name']
+            test_url = x['query']
+            test_check = x['result']
+            print('Testing: '+test_name)
+            r = requests.get(url = test_url)
+            if test_check in r.content.decode("utf-8"):
+                print("PASSED")
+            else:
+                print("FAIL")
+    except yaml.YAMLError as exc:
+        print(exc)
