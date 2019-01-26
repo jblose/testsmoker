bd2f85b3256eb6b3aa6f28e99da032349453df49
jblose <jamblose@gmail.com>
2019-01-26 13:28:38 -0500

    Migrated to FLASK app

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import requests
 import yaml
+from flask import Flask
 
-with open("tests.yml", 'r') as stream:
-    try:
-        testyaml = yaml.safe_load(stream)
-        for x in testyaml['tests']:
-            test_name = x['name']
-            test_url = x['query']
-            test_check = x['result']
-            print('Testing: '+test_name)
-            r = requests.get(url = test_url)
-            if test_check in r.content.decode("utf-8"):
-                print("PASSED")
-            else:
-                print("FAIL")
-    except yaml.YAMLError as exc:
-        print(exc)
+app = Flask(__name__)
+
+@app.route('/')
+def smoketestendpoint():
+    return runtests()
+
+def runtests():
+    results = ''
+    with open("tests.yml", 'r') as stream:
+        try:
+            testyaml = yaml.safe_load(stream)
+            for x in testyaml['tests']:
+                test_name = x['name']
+                test_url = x['query']
+                test_check = x['result']
+                results += "# Testing: " + test_name + " \n"
+                r = requests.get(url = test_url)
+                results += test_name.lower().replace(" ","_")
+                if test_check in r.content.decode("utf-8"):
+                    results += "PASSED" + " \n"
+                else:
+                    results += "FAIL" + " \n"
+        except yaml.YAMLError as exc:
+            results = exc
+    return results
