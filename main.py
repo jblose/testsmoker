4c07cf1daf4ba0c152397f463adca3e6bec6f928
jblose <jamblose@gmail.com>
2019-01-29 18:06:03 -0500

    Resolved issues with prometheus scrape

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests
 import yaml
 from flask import Flask
+from flask import Response
 
 app = Flask(__name__)
 
 @app.route('/metrics')
 def smoketestendpoint():
-    return runtests()
+    return Response(runtests(), mimetype='text/plain')
 
 def runtests():
-    results = '<pre style-"word-wrap: break-word; white-space: pre-wrap;">'
+    results = ''
     with open("tests.yml", 'r') as stream:
         try:
             testyaml = yaml.safe_load(stream)
@@ -17,14 +18,15 @@ def runtests():
                 test_name = x['name']
                 test_url = x['query']
                 test_check = x['result']
-                # results += "# Testing: " + test_name + '\n'
+                test_name_formed = test_name.lower().replace(" ","_")
+                results += "# HELP " + test_name_formed + " " + test_name_formed + '\n'
+                results += "# TYPE " + test_name_formed + " counter" + '\n'
                 r = requests.get(url = test_url)
-                results += test_name.lower().replace(" ","_") + " "
+                results += test_name_formed + " "
                 if test_check in r.content.decode("utf-8"):
                     results += "1" + '\n'
                 else:
                     results += "0" + '\n'
-            results += "</pre>"   
         except yaml.YAMLError as exc:
             results = exc
     return results
