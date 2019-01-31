cf55c75f6daa663e77eafef8345e04e1c24fb8af
Blose, Jamison <Jamison.Blose@wolterskluwer.com>
2019-01-31 14:00:35 -0500

    Addition of Test Groups to assist in grouping.

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ app = Flask(__name__)
 
 @app.route('/metrics')
 def smoketestendpoint():
-    return Response(runtests(), mimetype='text/plain')
+    return Response(run_test_groups(), mimetype='text/plain')
 
-def runtests():
+def run_test_groups():
     results = ''
     with open("tests.yml", 'r') as stream:
         try:
             testyaml = yaml.safe_load(stream)
-            for x in testyaml['tests']:
-                test_name = x['name']
-                test_url = x['query']
-                test_check = x['result']
-                test_name_formed = test_name.lower().replace(" ","_")
-                results += "# HELP " + test_name_formed + " " + test_name_formed + '\n'
-                results += "# TYPE " + test_name_formed + " counter" + '\n'
-                r = requests.get(url = test_url)
-                results += test_name_formed + " "
-                if test_check in r.content.decode("utf-8"):
-                    results += "1" + '\n'
-                else:
-                    results += "0" + '\n'
+            for tg in testyaml['testgroups']:
+                results += run_sub_tests(tg)
         except yaml.YAMLError as exc:
             results = exc
     return results
+
+def run_sub_tests(testyaml):
+    localresults = ''
+    for x in testyaml['tests']:
+        test_name = x['name']
+        test_url = x['query']
+        test_check = x['result']
+        test_name_formed = test_name.lower().replace(" ","_")
+        localresults += "# HELP " + test_name_formed + " " + test_name + '\n'
+        localresults += "# TYPE " + test_name_formed + " counter" + '\n'
+        r = requests.get(url = test_url)
+        localresults += test_name_formed + " "
+        if test_check in r.content.decode("utf-8"):
+            localresults += "1" + '\n'
+        else:
+            localresults += "0" + '\n'
+    return localresults
 
 if __name__ == '__main__':
     app.run()
