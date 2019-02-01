8dfc45e31c274ecc17e06db4be9441a73f1c1a1c
jblose <jamblose@gmail.com>
2019-01-31 21:26:32 -0500

    Added comments like an adult.

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from flask import Response
 
 app = Flask(__name__)
 
+# Route for execution
 @app.route('/metrics')
 def smoketestendpoint():
     return Response(run_test_groups(), mimetype='text/plain')
 
 def run_test_groups():
     results = ''
+    # Open YML file
     with open("tests.yml", 'r') as stream:
         try:
+            # safe_load due to vulnerabilities
             testyaml = yaml.safe_load(stream)
+            # Iterate on testgroups
             for tg in testyaml['testgroups']:
                 group_name = tg['name']
                 group_name_formed = group_name.lower().replace(" ","_")
                 passed_cnt = 0
-                total_cnt = 0                
+                total_cnt = 0      
+                # Iterate on tests inside testgroups          
                 for x in tg['tests']:
                     total_cnt += 1
                     test_name = x['name']
@@ -29,18 +34,24 @@ def run_test_groups():
                     results += "# TYPE " + test_name_formed + " counter" + '\n'
                     r = requests.get(url = test_url)
                     results += test_name_formed + " "
+                    # Passed tests are "true/1, failures our "false/0"
                     if test_check in r.content.decode("utf-8"):
                         passed_cnt += 1
                         results += "1" + '\n'
                     else:
-                        results += "0" + '\n'            
+                        results += "0" + '\n'       
+
+                # Total tests executed for a group
                 results += "# HELP " + group_name_formed + "_total " + group_name + " Total" + '\n'
                 results += "# TYPE " + group_name_formed + "_total counter" + '\n'
                 results += group_name_formed + "_total " + str(total_cnt) + '\n'
+                
+                # Total Passed tests executed for the group
                 results += "# HELP " + group_name_formed + "_passed_total " + group_name + " Total" + '\n'
                 results += "# TYPE " + group_name_formed + "_passed_total counter" + '\n'
                 results += group_name_formed + "_passed_total " + str(passed_cnt) + '\n'
                 
+                # Total Failed tests executed for the group
                 failed_cnt = total_cnt - passed_cnt
                 results += "# HELP " + group_name_formed + "_failed_total " + group_name + " Total" + '\n'
                 results += "# TYPE " + group_name_formed + "_failed_total counter" + '\n'
